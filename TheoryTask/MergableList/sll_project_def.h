8e2c028597bb6719b57d504f52e143cc264f69f4
Wendy-Hamlet <shxx110130@163.com>
2025-12-16 09:34:14 +0800

    revise format

--- TheoryTask/MergableList/sll_project_def.h
+++ TheoryTask/MergableList/sll_project_def.h
@@ -10,21 +10,24 @@ struct sllb {
 
 /*@ Import Coq Require Import sll_project_lib */
 
+// sll predicates
 /*@ Extern Coq (sll : Z -> list Z -> Assertion) */
 /*@ Extern Coq (sllseg : Z -> Z -> list Z -> Assertion) */
 /*@ Extern Coq (sllbseg : Z -> Z -> list Z -> Assertion) */
 /*@ Extern Coq (sllb : Z -> list Z -> Assertion) */
 /*@ Extern Coq (map_mult : Z -> list Z -> list Z) */
 
-/*@ Extern Coq (UIntArray::full : Z -> Z -> list Z -> Assertion)
-               (UIntArray::missing_i : Z -> Z -> Z -> Z -> list Z -> Assertion)
-               (UIntArray::undef_full : Z -> Z -> Assertion)
-               (UIntArray::undef_ceil : Z -> Z -> Z -> Assertion)
-               (UIntArray::undef_missing_i : Z -> Z -> Z -> Z -> Assertion)
-               (UIntArray::ceil_shape : Z -> Z -> Z -> Assertion)
-               (UIntArray::full_shape : Z -> Z -> Assertion)
-               (Znth : {A} -> Z -> list A -> A -> A)
-               (replace_Znth : {A} -> Z -> A -> list A -> list A)
-*/
+// UIntArray predicates
+/*@ Extern Coq (UIntArray::full : Z -> Z -> list Z -> Assertion) */
+/*@ Extern Coq (UIntArray::missing_i : Z -> Z -> Z -> Z -> list Z -> Assertion) */
+/*@ Extern Coq (UIntArray::undef_full : Z -> Z -> Assertion) */
+/*@ Extern Coq (UIntArray::undef_ceil : Z -> Z -> Z -> Assertion) */
+/*@ Extern Coq (UIntArray::undef_missing_i : Z -> Z -> Z -> Z -> Assertion) */
+/*@ Extern Coq (UIntArray::ceil_shape : Z -> Z -> Z -> Assertion) */
+/*@ Extern Coq (UIntArray::full_shape : Z -> Z -> Assertion) */
+
+// Utility functions
+/*@ Extern Coq (Znth : {A} -> Z -> list A -> A -> A) */
+/*@ Extern Coq (replace_Znth : {A} -> Z -> A -> list A -> list A) */
 
 /*@ include strategies "sll_project.strategies" */
